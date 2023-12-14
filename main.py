d951d764deecc7d8452246a077664e93dd0463af
md-invt05 <92951375+md-invt05@users.noreply.github.com>
2023-12-14 20:54:33 +0300

    first try

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
 import mysql.connector
+import datetime
 
 mydb = mysql.connector.connect(
     host="localhost",
-    user="yourusername",
-    password="yourpassword",
-    database="yourdatabase"
+    port="3306",
+    user="root",
+    password="qwerty",
+    database="systemofzaselenie"
 )
 
+# adding new data to db
+def add_student(data):
+    with mydb.cursor() as cursor:
+        insert_student = """
+                INSERT INTO студент (Имя, ДатаРождения, Фото,
+                НомерПриказаОПредоставленииОбщежития, НомерПриказаОЗачислении, ДатаЗачисления,
+                МестоРождения,АдресПроживания,Фамилия,Отчество)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """
+        cursor.execute(insert_student, data)  # Use execute() instead of executemany() since there's only one set of values
+        mydb.commit()
 
-def search_students(corpus, floor, room):
-    mycursor = mydb.cursor()
-    query = "SELECT student.name " \
-            "FROM student " \
-            "JOIN corpus ON student.corpus_id = corpus.id " \
-            "JOIN floor ON student.floor_id = floor.id " \
-            "JOIN room ON student.room_id = room.id " \
-            "WHERE corpus.name = %s " \
-            "AND floor.name = %s " \
-            "AND room.name = %s"
-    values = (corpus, floor, room)
-    mycursor.execute(query, values)
-    result = mycursor.fetchall()
-    mycursor.close()
-    return result
+#delete data
+def delete_students(id):
+    with mydb.cursor() as cursor:
+        deletestudent = f"""
+        DELETE FROM студент WHERE НомерКарточки = {id}
+        """
+        cursor.execute(deletestudent)
+        mydb.commit()
+with open('wp9450562.jpg', 'rb') as file:
+    photo_data = file.read()
 
-
-
-
-students = search_students("Корпус 1", "Этаж 2", "Комната 101")
-for student in students:
-    print(student[0])
+data = [
+    "gg", datetime.date(2000, 10, 10), "wp9450562.jpg", 8274242424324, 234242433234, datetime.date(2020, 10, 10),
+     "ggg", "sfsfsf", "gg", "gg"
+]
+#add_student(data)
+delete_students(2)
+"""name, date_of_birth, photo, num_of_order_accom,
+                num_of_order_enrollment, date_enrollment, place_birth,
+                place_of_living, surname, patronymic, num_of_room"""
