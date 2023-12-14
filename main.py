8eb0dfa2e2cfd9305d6d617f97d57bc3ee6924c9
md-invt05 <92951375+md-invt05@users.noreply.github.com>
2023-12-14 23:05:27 +0300

    some updates

--- main.py
+++ main.py
@@ -29,15 +29,3 @@ def delete_students(id):
         """
         cursor.execute(deletestudent)
         mydb.commit()
-with open('wp9450562.jpg', 'rb') as file:
-    photo_data = file.read()
-
-data = [
-    "gg", datetime.date(2000, 10, 10), "wp9450562.jpg", 8274242424324, 234242433234, datetime.date(2020, 10, 10),
-     "ggg", "sfsfsf", "gg", "gg"
-]
-#add_student(data)
-delete_students(2)
-"""name, date_of_birth, photo, num_of_order_accom,
-                num_of_order_enrollment, date_enrollment, place_birth,
-                place_of_living, surname, patronymic, num_of_room"""
